5bb08977c7859031343f36120a30ab8a2605a9a5
rustam023 <r.91@mail.ru>
2021-03-23 23:34:30 +0300

    final2

---
+++ app/frontend/components/contacts/Contacts.js
@@ -0,0 +1,25 @@
+import React, { Fragment, useState, useEffect } from 'react'
+import axios from 'axios'
+
+import ContactItem from './ContactItem'
+
+const Contacts = () => {
+  const [contacts, setContacts] = useState(null)
+
+  const getContacts = async () => {
+    const { data } = await axios.get('api/v1/contacts')
+    setContacts(data.data)
+  }
+
+  useEffect(() => {
+    getContacts()
+  }, [])
+
+  return (
+    <Fragment>
+      {contacts && contacts.map(contact => <ContactItem key={contact.id} name={contact.name} email={contact.email}/>)}
+    </Fragment>
+  )
+}
+
+export default Contacts
