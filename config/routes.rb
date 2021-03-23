39ca19bb4732e0e5fc933d73943f451729a58594
rustam023 <r.91@mail.ru>
2021-03-23 19:48:35 +0300

    add contacts api

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   root 'index#index'
+
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      resources :contacts, only: %i[create index]
+    end
+  end
 end
