36d9969fe7b721da802ae09995a518aac30670f7
rustam023 <r.91@mail.ru>
2021-03-23 20:18:22 +0300

    add react i18next

--- app/frontend/components/Header.js
+++ app/frontend/components/Header.js
@@ -1,14 +1,17 @@
 import React from 'react'
 
-import { Navbar, Nav } from "react-bootstrap";
+import { Navbar, Nav } from 'react-bootstrap';
+import { useTranslation } from 'react-i18next';
 
 const Header = () => {
+  const { t, i18n } = useTranslation('common')
+
   return (
     <Navbar bg="light" expand="lg">
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mr-auto">
-          <Nav.Link href="#">Contacts</Nav.Link>
-          <Nav.Link href="#">Add contact</Nav.Link>
+          <Nav.Link href="#">{t('navi.contacts')}</Nav.Link>
+          <Nav.Link href="#">{t('navi.add_contact')}</Nav.Link>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
