111bb2bc69ca8fdf7ddd212eb5813219c1101b57
rustam023 <r.91@mail.ru>
2021-03-23 17:52:59 +0300

    should be initial

---
+++ app/frontend/components/App.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react'
+
+import './App.scss'
+
+const App = () => {
+  return (
+    <Fragment>
+      react on rails
+    </Fragment>
+  )
+}
+
+export default App
